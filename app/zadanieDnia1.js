cd4687381e9f4dd470516acc951a9899a544e4d7
Lucyna Mazur <lucyna@studiokreator.com>
2018-07-11 20:43:53 +0200

    ready

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,13 @@
 //Twój kod
+
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+	res.setHeader("Content-Type", "text/html; charset=utf-8");
+	res.end('Hello, World from back-end!');
+	
+});
+
+srv.listen(3000, () => {
+	console.log('serwer uruchomiony na porcie 3000');
+});
--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,13 @@
 //Twój kod
+
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+	const userAgentHeader = req.headers['user-agent'];
+	res.setHeader("Content-Type", "text/html; charset=utf-8");
+	res.end(userAgentHeader);
+});
+
+srv.listen(3000, () => {
+	console.log('serwer uruchomiony na porcie 3000');
+});
